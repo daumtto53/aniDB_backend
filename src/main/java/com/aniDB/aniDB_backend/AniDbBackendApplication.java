6ef15c1e0c493ebfbcfc6526961a4938084e3bb1
daumtto53 <daumtto53@gmail.com>
2024-08-14 18:37:06 +0900

    feat(init project): initiallize project.

---
+++ src/main/java/com/aniDB/aniDB_backend/AniDbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aniDB.aniDB_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniDbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniDbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aniDB/aniDB_backend/AniDbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniDB.aniDB_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniDbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
