327e587c5fbfa48a4e46cffbf83fa0ee77672ce7
daumtto53 <daumtto53@gmail.com>
2024-09-03 14:11:52 +0900

    feat(comment): article comment now can delete, modify.

--- src/main/java/com/aniDB/aniDB_backend/service/CommentService.java
+++ src/main/java/com/aniDB/aniDB_backend/service/CommentService.java
@@ -1,10 +1,15 @@
 package com.aniDB.aniDB_backend.service;
 
 import com.aniDB.aniDB_backend.dto.entity.comment.CommentDTO;
+import com.aniDB.aniDB_backend.dto.entity.member.MemberDTO;
+import com.aniDB.aniDB_backend.entity.Member;
 import com.aniDB.aniDB_backend.mapper.UpvotedCommentMapper;
 import com.aniDB.aniDB_backend.repository.CommentRepository;
+import com.aniDB.aniDB_backend.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +18,16 @@ import org.springframework.stereotype.Service;
 public class CommentService {
     private final CommentRepository commentRepository;
     private final UpvotedCommentMapper upvotedCommentMapper;
+    private final MemberRepository memberRepository;
 
     public CommentDTO createComment(Long articleId, CommentDTO commentDTO) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        Member member = memberRepository.findByUsername(username);
+
         commentDTO.setArticleId(articleId);
+        commentDTO.setMemberDTO(MemberDTO.builder().memberId(member.getMemberId()).build());
         // Member setting.
         commentRepository.createComment(commentDTO);
         return commentDTO;
